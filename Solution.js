6aebe378366cfa7671e7cca3d392250c4bf15741
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-01 22:25:00 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,18 @@
+
+/**
+ * @param {number} k
+ * @return {string}
+ */
+var kthLuckyNumber = function (k) {
+    const LUCKY_DIGITS = ['4', '7'];
+    let oneBasedIndexing = k + 1;
+    let length = Math.floor(Math.log2(oneBasedIndexing));
+    const kthLuckyNumber = new Array(length);
+
+    for (let i = length - 1; i >= 0; --i) {
+        kthLuckyNumber[i] = LUCKY_DIGITS[oneBasedIndexing & 1];
+        oneBasedIndexing >>= 1;
+    }
+
+    return kthLuckyNumber.join('');
+};
